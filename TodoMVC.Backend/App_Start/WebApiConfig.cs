07b65fcd7f03ae56036b63a8a992849e859807fd
Micha Alt <mail@lean-stack.de>
2016-02-23 20:28:47 +0100

    WebAPI Controller hinzugefügt. Und Routing konfiguiert.

---
+++ TodoMVC.Backend/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TodoMVC.Backend
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "TodoApi",
+                routeTemplate: "api/todos/{id}",
+                defaults: new { id = RouteParameter.Optional, controller = "WebAPITodos" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TodoMVC.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TodoMVC.Backend.WebApiApplication" Language="C#" %>
