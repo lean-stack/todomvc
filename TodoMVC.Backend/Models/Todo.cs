2f6835077bce8b571e2183d784effa300d176aa5
Micha Alt <mail@lean-stack.de>
2016-02-23 20:21:59 +0100

    Entity Framework 6 Code First Modell hinzugefügt.

---
+++ TodoMVC.Backend/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoMVC.Backend.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Txt { get; set; }
+        public bool Done { get; set; }
+    }
+}
