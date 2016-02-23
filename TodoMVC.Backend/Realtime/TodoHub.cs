03d7650b0680b1f2d6cc829b70f8a65850d13f29
Micha Alt <mail@lean-stack.de>
2016-02-23 21:37:24 +0100

    Signalr Hub added.

---
+++ TodoMVC.Backend/Realtime/TodoHub.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using TodoMVC.Backend.Controllers;
+using TodoMVC.Backend.Models;
+
+namespace TodoMVC.Backend.Realtime
+{
+    public class TodoHub : Hub
+    {
+        TodoDbContext ctx = new TodoDbContext();
+
+        public void GetAll()
+        {
+            Clients.Caller.sentAll(ctx.Todos.ToList());
+        }
+
+        public void CreateTodo(string txt)
+        {
+            var todo = new Todo() { Txt = txt };
+            ctx.Todos.Add(todo);
+            ctx.SaveChanges();
+
+            Clients.All.createdTodo(todo);
+        }
+
+        public void UpdateTodo(int id, string txt, bool done)
+        {
+            var todo = ctx.Todos.Find(id);
+            todo.Txt = txt;
+            todo.Done = done;
+            ctx.SaveChanges();
+
+            Clients.All.updatedTodo(todo);
+        }
+
+        public void DeleteTodo(int id)
+        {
+            Todo todo = ctx.Todos.Find(id);
+            ctx.Todos.Remove(todo);
+            ctx.SaveChanges();
+            Clients.All.deletedTodo(id);
+
+        }
+    }
+}
---
+++ TodoMVC.Backend/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TodoMVC.Backend.Startup))]
+
+namespace TodoMVC.Backend
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
